d5f1fc5b3cf49d4e7d1c898d50f970424dbb2721
YukiSato <yukiyukiamam@gmail.com>
2022-12-30 23:17:43 +0900

    [add]initial app

---
+++ stockroom-api/src/test/java/com/stockroom/stockroom/StockroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockroom.stockroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
