df7cd78d53f1b10d6b0202b7dc2bd5e7ec715075
YukiSato <yukiyukiamam@gmail.com>
2023-01-02 19:19:37 +0900

    [fix]簡単なuser API

--- stockroom-api/src/main/java/com/stockroom/domain/model/User.java
+++ stockroom-api/src/main/java/com/stockroom/domain/model/User.java
@@ -1,8 +1,9 @@
 package com.stockroom.domain.model;
 
 import com.stockroom.util.Function;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.security.NoSuchAlgorithmException;
 import java.sql.Timestamp;
@@ -10,11 +11,13 @@ import java.util.UUID;
 
 @Getter
 @RequiredArgsConstructor
+@NoArgsConstructor(force = true)
 public class User {
     private final UUID id;
     private final String email;
     private String token;
     private final String passwordDigest;
+    private Timestamp activatedAt;
     private final Boolean activated;
     private final Integer status;
     private Timestamp createdAt;
